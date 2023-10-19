e9d572f5440e035311b6c7dc85cf3156a319111e
phulloux <phulloux2004@gmail.com>
2023-10-19 08:57:56 +0200

    terminer

--- src/main/java/fr/exo2if/App.java
+++ src/main/java/fr/exo2if/App.java
@@ -1,19 +1,19 @@
 package fr.exo2if;
 
+import java.util.Scanner;
+
 public class App {
     
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        int salaireAnnuel;
 
         System.out.println("Travailez vous depuis moins de 2 ans : ");
-
         boolean travailMoins2Ans = clavier.nextBoolean();
 
         System.out.println("Combien gagnez vous par an : ");
-        int salaireAn = clavier.nextInt();
+        int salaireAnnuel = clavier.nextInt();
 
-        if(travailMoins2Ans == true && salaireAn <= 30000){
+        if(travailMoins2Ans == true && salaireAnnuel <= 30000){
             System.err.println("Vous pouvez avoir un prêt");
         }
         else{
